54ca596ec1c09553d2ec6883ed10f5afab63a4b5
pushpendra <pushpendrauikey963@gmail.com>
2025-05-25 22:04:12 +0530

    All the sorting algorithms

---
+++ Array-Search-Sort/AllSortingAlgo.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Selection sort: Sort from the beginning i.e. 0th index
+void selectionSort(vector<int> &arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+
+// Bubble sort: Sort from the end i.e. n-1th index, keep pushing max element in the end.
+void bubbleSort(vector<int> &arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break; // Optimized: Stop if already sorted
+    }
+}
+
+// Insertion sort: Keep inserting the element in right place, from the beginning.
+void insertionSort(vector<int> &arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// Merge Sort
+void merge(vector<int> &arr, int left, int mid, int right) {
+    vector<int> temp(right - left + 1);
+    int i = left, j = mid + 1, k = 0;
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+        }
+    }
+    while (i <= mid) temp[k++] = arr[i++];
+    while (j <= right) temp[k++] = arr[j++];
+    for (int l = 0; l < temp.size(); l++) {
+        arr[left + l] = temp[l];
+    }
+}
+
+void mergeSort(vector<int> &arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+// Quick Sort
+int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int> &arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+// Counting Sort
+void countingSort(vector<int> &arr) {
+    int maxVal = *max_element(arr.begin(), arr.end());
+    int minVal = *min_element(arr.begin(), arr.end());
+    int range = maxVal - minVal + 1;
+
+    vector<int> count(range, 0);
+    vector<int> output(arr.size());
+    
+    for (int num : arr) count[num - minVal]++;
+    for (int i = 1; i < range; i++) count[i] += count[i - 1];
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        output[--count[arr[i] - minVal]] = arr[i];  // pre-decrementing adjusts it to zero based index.
+    }
+    arr = output;
+}

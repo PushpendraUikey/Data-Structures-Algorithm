1931b19266693c645864da48c39a4d2b11377b73
pushpendra <pushpendrauikey963@gmail.com>
2025-07-28 14:13:35 +0530

    Few more approaches to find the longest substring without repeating characters

--- String/longSubstrWithtRepeat.cpp
+++ String/longSubstrWithtRepeat.cpp
@@ -4,6 +4,8 @@ using namespace std;
 /*
 Algorithm to find the unique substring in the given input string(maximum)
 Return: maximum possible unique substring length(that does not have any repeating charac)
+
+This problem is also referred as finding longest substring with distinct characters
 */
 int uniqueSubstrings(string input)
 {
@@ -27,4 +29,118 @@ int uniqueSubstrings(string input)
     }
 
     return maxLen;
+}
+
+
+// First O(n^3) solution
+bool Aredistinct(const string&str, int i, int j){
+    vector<bool> visited(256, false);
+    for(int k=i; k<=j; k++){
+        if(visited[str[k]]==true){
+            return false;
+        }
+        visited[str[k]] = true;
+    }
+    return true;
+}
+int longestDistinct(const string& str){
+    int n = str.length();
+    int res = 0;
+    for(int i=0; i<n; i++){
+        for(int j=i; j<n; j++){
+            if(Aredistinct(str, i, j)){
+                res = max(res, j-i+1);
+            }
+        }
+    }
+    return res;
+}
+
+// Here is O(n^2) solution to the above problem of longest Distinct substring
+int longestDistinct_eff(const string& str){
+    int n = str.length();
+    int res = 0;
+
+    for(int i=0; i<n; i++){
+        vector<bool> visited(256, false);
+        for(int j=i; j<n; j++){
+            if( visited[str[j]] == true) break;
+            else{
+                res = max(res, j-i+1);
+                visited[str[j]] = true;
+            }
+        }
+    }
+}
+
+// Here's the O(n) Optimized solution like the first solution in this file
+int longestDistinct_opt(const string& str){
+    int n = str.length();
+    int i = 0;
+    int res = 0;
+    vector<int> lastidx(256, -1);
+    for(int j=0; j<n; j++){
+        i = max(i, lastidx[str[j]]+1);
+        int maxend = j-i+1;
+        res = max(res, maxend);
+        lastidx[str[j]] = j;
+    }
+    return res;
+}
+
+
+// Credit: GfG
+/*
+Method 5 (Linear time):   In this method we will apply  KMP Algorithm technique, to solve the problem. 
+We maintain an Unordered Set to keep track of the maximum non repeating char sub string (Instead of 
+standard LPS array of KMP). When ever we find a repeating char, then we clear the Set and reset len 
+to zero. Rest everything is almost similar to KMP.
+*/
+int longestSubstrDistinctChars(string s)
+{
+	if (s.length() == 0)
+		return 0;
+	int n = s.length();
+	set<char> st;
+	int len = 1;
+	st.insert(s[0]);
+	int i = 1;
+	int maxLen = 0;
+	while (i < n)
+	{
+		if (s[i] != s[i - 1] && st.find(s[i]) == st.end())
+		{
+			st.insert(s[i]);
+			len++;
+			i++;
+			if (len > maxLen)
+			{
+				maxLen = len;
+			}
+		}
+		else
+		{
+			if (len == 1)
+			{
+				i++;
+			}
+			else
+			{
+				st.clear();
+				i = i - len + 1;
+				len = 0;
+			}
+		}
+	}
+	return max(maxLen, len);
+}
+
+
+int main()
+{
+	string str = "abcabcbb";
+	cout << "The input string is " << str << endl;
+	int len = longestSubstrDistinctChars(str);
+	cout << "The length of the longest non-repeating character substring " << len;
+	return 0;
 }

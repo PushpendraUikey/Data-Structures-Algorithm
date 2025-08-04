cb621c52363719807a83c0cb3b6be1f235123445
pushpendra <pushpendrauikey963@gmail.com>
2025-08-04 12:26:11 +0530

    Minimum Jumps to reach the end using recursive approach

--- Dynamic-Programming/minimumJumpToreachEnd.cpp
+++ Dynamic-Programming/minimumJumpToreachEnd.cpp
@@ -61,3 +61,17 @@ class Solution {
         return -1;
     }
 };
+
+// Recursive solution to the Minimum Jumps to reach end!
+int minJumps(int arr[], int n){
+    if(n==1) return 0;
+    int res = INT_MAX;
+    for(int i=0; i<=n-2; i++){
+        if(i+arr[i] >= n-1){
+            int sub_res = minJumps(arr, i+1);
+            if(sub_res != INT_MAX) 
+                res = min(res, sub_res+1);
+        }
+    }
+    return res;
+}

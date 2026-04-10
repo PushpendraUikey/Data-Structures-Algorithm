0b25d07898c82476249deb506f7b3090184bbb7e
Pushpendra Uikey <144332328+PushpendraUikey@users.noreply.github.com>
2026-04-11 01:38:23 +0530

    Add longestPalindrome function using center expansion
    Implemented a solution to find the longest palindromic substring using the expand around center approach.

--- String/Longest_Palindrome.cpp
+++ String/Longest_Palindrome.cpp
@@ -64,6 +64,29 @@ public:
     }
 };
 
+// Alternative 1: Expand across a center
+class Solution {
+    string expand(string s, int l, int r){
+        while(l>=0 && r<s.size() && s[l] == s[r]) {
+            l--;
+            r++;
+        }
+        return s.substr(l+1, r-l-1);
+    }
+public:
+    string longestPalindrome(string s) {
+        string res = "";
+        for(int i=0; i<s.size(); i++) {
+            string odd = expand(s, i, i);
+            string even = expand(s, i, i+1);
+            if(odd.size() > res.size()) res = odd;
+            if(even.size() > res.size()) res = even;
+        }
+
+        return res;
+    }
+};
+
 int main(){
     string s;
     cin >> s;
